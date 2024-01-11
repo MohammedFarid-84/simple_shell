8fd9f6fa65b064d13754c7f2a6c0684bd65327ef
Elhlawany <aboabdallahhlawany@gmil.com>
2024-01-11 21:53:48 +0200

    remove test derictory

---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* NESSESRY LIBRARIES */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+
+/* PROTOTYPE FUNCTIONS */
+char *erromsg(int, char *, char *);
+char **splitLine(char *, char *);
+void execCmd(int, char *);
+void printtxt(char *);
+void show_shell(unsigned int);
+char **splitLine(char *, char *);
+
+
+#endif
